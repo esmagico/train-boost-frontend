1a5152ad41105c747ad38d807ca37ab4794a4715
AnkitEsmagico <ankit.k@esmagico.in>
2025-07-11 17:13:49 +0530

    presention link fix

--- src/app/page.js
+++ src/app/page.js
@@ -17,14 +17,18 @@ const Home = () => {
         <div className="flex flex-1 p-6 overflow-hidden">
           {!isQuestionMode ? (
             <div className="flex w-full h-full gap-6">
-              <VideoPanel videos={videos} loading={isLoading}/>
-              <PPTSection loading={isLoading} presentationUrl="https://docs.google.com/presentation/d/1yyZtqREBI0fS6zZ2HlKMwGnrUwO6VXab/edit?slide=id.p1#slide=id.p1" />
+              <VideoPanel videos={videos} loading={isLoading} />
+              <PPTSection
+                loading={isLoading}
+                presentationUrl="https://docs.google.com/presentation/d/1yyZtqREBI0fS6zZ2HlKMwGnrUwO6VXab/edit?slide=id.p1#slide=id.p1"
+              />
             </div>
           ) : (
             <div className="flex w-full h-full gap-6 transition-all duration-300">
               <div className="flex flex-col w-[70%] h-full">
                 <div className="bg-white rounded-xl h-[calc(100vh-120px)] transition-all duration-300">
                   <PPTSection
+                    presentationUrl="https://docs.google.com/presentation/d/1yyZtqREBI0fS6zZ2HlKMwGnrUwO6VXab/edit?slide=id.p1#slide=id.p1"
                     removeAskQuestionButton={true}
                     isQuestionMode={true}
                     height="calc(100vh - 120px)"
